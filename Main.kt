fae9210049fdbe4681b2fa50c2f95aaea31dc8f7
alcinacarlos <calcrom0607@g.educaand.es>
2024-02-20 20:43:45 +0100

    2.2 commit

--- Main.kt
+++ Main.kt
@@ -1,4 +1,6 @@
 fun main(){
     println("Hola Familia, bienvenidos a este tutorial de GIT facilito pa to ustedes")
     println("Cambio 2 ")
+    println("Cambio 2.1")
+    println("Cambio 2.2")
 }
